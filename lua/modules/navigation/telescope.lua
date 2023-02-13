cad64d344bbb87f1702f0e96e80153c5b4b5b086
leonardSA <leonard.stephenauguste@gmail.com>
2023-02-13 11:59:48 +0100

    modules/telescope.lua: map fun find_files_open to find_files picker as select

--- lua/modules/navigation/telescope.lua
+++ lua/modules/navigation/telescope.lua
@@ -12,7 +12,37 @@ local actions = require('telescope.actions')
 vim.keymap.set('n', '<C-p>', builtin.find_files)
 vim.keymap.set('n', '<A-p>g', builtin.live_grep)
 -- buffers binds
-vim.keymap.set('n', '<A-p>t', builtin.buffers)
+vim.keymap.set('n', '<A-p>b', builtin.buffers)
+------------------------------------------------------------------------------- 
+
+------------------------------------------------------------------------------- 
+-- Private functions
+
+-- @brief Opens a file in a new buffer if it is not already opened else switches to the opened buffer.
+-- @prompt_bufnr current buffer aka buffer in which to open the file if it is not opened yet
+local function find_files_open(prompt_bufnr) 
+    local entry = require('telescope.actions.state').get_selected_entry()
+    -- get absolute path to file to compare with bufname
+    local filename = entry['cwd'] .. '/' .. entry.value
+
+    -- look for a corresponding buffer through every windows of every tab
+    local tabs = vim.api.nvim_list_tabpages()
+    for _, tab in ipairs(tabs) do
+        local windows = vim.api.nvim_tabpage_list_wins(tab)
+        for _, window in ipairs(windows) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(window))
+            if filename == bufname then -- switch to tab where file is opened and exit
+                vim.api.nvim_set_current_tabpage(tab)
+                vim.api.nvim_set_current_win(window)
+                return
+            end
+        end
+    end
+
+    -- file is not opened yet
+    actions.select_default(prompt_bufnr)
+end
+
 ------------------------------------------------------------------------------- 
 
 require('telescope').setup({
@@ -47,6 +77,10 @@ require('telescope').setup({
         },
         find_files = {
             prompt_title = 'Search Project',
+            mappings = {
+                i = { ["<CR>"] = find_files_open, },
+                n = { ["<CR>"] = find_files_open, },
+            },
         },
         oldfiles = {
             prompt_title = 'Search File History',
